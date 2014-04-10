0fe3c9f775541325c435e7c3323504c0182ed528
Anaibol <juananibal.micheli@gmail.com>
2014-04-10 14:08:31 -0300

    new cron searches

--- cron.js
+++ cron.js
@@ -3,6 +3,10 @@ var request = require('request')
   , async = require('async')
   , format = require('util').format;
   
+var config = require('./config/config');
+var db = require('monk')(config.db);
+var Events = db.get('events');
+
 var winston = require('winston');
 
 winston.loggers.add('eventNames', {
@@ -35,7 +39,7 @@ var graph = require('fbgraph');
 var accessToken = 'CAAGPsrwaxr4BADGXeox8qWYOtUe14RLbobooZA4DMJEzReUROPJvaxbnBzI3LGgNAn9qfDUefXGZBZBzZBXwxWgw3ZCyAhKe5qZAKEAveKo9VhzdOEEceUquxWaFrlEdPwXfJKEZBAnXqI8MeprXVGrCPaqJUfpUqZCkZBZBEjWpUvNoPQxE07tINZAjKSwuM34U8wZD';
 graph.setAccessToken(accessToken);
 
-var words = ['salsa', 'bachata', 'kizomba', 'porto', 'cubaine', 'cubana', 'semba', 'samba', 'merengue', 'tango'];
+var keywords = ['salsa', 'bachata', 'kizomba', 'porto', 'cubaine', 'cubana', 'semba', 'samba', 'merengue', 'tango', 'lambazouk', 'regueton', 'reggaeton'];
 var users = [ 'EsenciaSalsaClub',
 'clubastoriabcn',
 'SevenDanceEscuelaBaile',
@@ -56,21 +60,21 @@ var users = [ 'EsenciaSalsaClub',
 'MangosTropCafe'];
 
 var cronJob = require('cron').CronJob;
-
+var newEvents;
 
 
 // new cronJob('*/30 * * * * ', function() {
+  newEvents = 0;
   var date = new Date();
   console.log(date.toString());
 
-  for (var i = 0; i < words.length; i++) {
-    search(words[i], function(numEvents){
-      console.log(numEvents);
-    });
-  }
 
-  // updateAttendees();
-  // updateEventsFromUsers();
+  fetchEventsFromKeywords();
+
+  //updateAttendees();
+  fetchEventsFromUsers();
+  fetchEventsFromUsers2();
+  fetchEventsFromLocations();
 
 // }, null, true);
 
@@ -80,7 +84,7 @@ function paginate(page, term) {
       if (res.paging && res.paging.next) {
         paginate(res.paging.next, term);
       }
-      console.log(res);
+
       var events = res.data;
 
       events.forEach(function(ev) {
@@ -92,13 +96,13 @@ function paginate(page, term) {
   });
 }
 
-var config = require('./config/config');
-var db = require('monk')(config.db);
-var Events = db.get('events');
+function fetchEventsFromKeywords() {
+  for (var i = 0; i < keywords.length; i++) {
+    searchEventsFromKeyword(keywords[i]);
+  }
+}
 
-function search(term, cb) {
-  var newEvents = 0;
-
+function searchEventsFromKeyword(term, cb) {
   var searchOptions = {
     q: term,
     type: 'event',
@@ -127,7 +131,7 @@ function search(term, cb) {
         async.each(events, function(ev, cb){
           fetchEvent(ev.id, term, function(ev){
             newEvents++;
-            eventNames.info(term + ': ' + ev.name);
+            console.log(term + ': ' + ev.name);
             cb();
           });
         }, function(err) {
@@ -140,10 +144,9 @@ function search(term, cb) {
 
 function fetchEvent(id, term, cb) {
   id = parseInt(id);
-
+            
   existsInDb(id, function(exists) {
     if (!exists) {
-
       var query = {
         user_event: "SELECT description, eid, location, name, privacy, start_time, end_time, update_time, ticket_uri, venue, pic, pic_big, pic_small, pic_square, pic_cover, has_profile_pic, pic, creator, timezone FROM event WHERE eid =" + id,
         event_attending: "SELECT uid FROM event_member WHERE eid IN (SELECT eid FROM #user_event) and rsvp_status = 'attending' LIMIT 50000",
@@ -174,8 +177,6 @@ function fetchEvent(id, term, cb) {
 
             eve.start_time = new Date(Date.parse(eve.start_time));
             eve.end_time = new Date(Date.parse(eve.end_time));
-
-            //eve.location = (eve.venue.location) ? eve.venue.location : '' + (eve.venue.city) ? eve.venue.city : '' + (eve.venue.state) ? eve.venue.state : '' + (eve.venue.state) ? eve.venue.state : '';
 
             eve.place = [];
 
@@ -239,12 +240,12 @@ function getTags(eve) {
   var text = name + ' ' + desc;
   text = text.toLowerCase();
 
-  for (var i = 0; i < words.length; i++) {
-    var str = words[i];
+  for (var i = 0; i < keywords.length; i++) {
+    var str = keywords[i];
     var n = text.search(str);
 
     if (n > 0) {
-      tags.push(words[i]);
+      tags.push(keywords[i]);
     }
   }
 
@@ -299,42 +300,104 @@ String.prototype.removeAll = function(target) {
   return this.split(target).join('');
 };
 
-function updateEventsFromUsers() {
-  async.each(users, function (user, next) {
-    var options = {
-      url: format('http://facebook.com/%s/events', user),
-      headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36'
-      }
-    };
+function crawlUserEvents(userName, cb) {
+  var options = {
+    url: format('http://facebook.com/%s/events', userName),
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36'
+    }
+  };
 
-    request(options, function (err, response, body) {
-      if (err) throw err;
+  request(options, function (err, response, body) {
+    if (err) throw err;
 
-      body = body.removeAll('<!--').removeAll('-->');
+    body = body.removeAll('<!--').removeAll('-->');
 
-      var $ = cheerio.load(body);
+    var $ = cheerio.load(body);
 
-      var html = $('.fbTimelineEvents').html();
+    var html = $('.fbTimelineEvents').html();
 
-      if (html) {
-        var $2 = cheerio.load(html);
+    if (html) {
+      var $2 = cheerio.load(html);
 
-        $2('.eventsGrid').each(function(i, elem) {
-          var url = $2(this).find('a').attr('href');
-          var re = /\d+/i;
-          var id = url.match(re);
-          fetchEvent(id[0], '');
+      $2('.eventsGrid').each(function(i, elem) {
+        var url = $2(this).find('a').attr('href');
+        var re = /\d+/i;
+        var id = url.match(re);
+
+        fetchEvent(id[0], '', function(){
+          newEvents++;
         });
-      }
+      });
+    }
+    cb();
+  });
+}
 
+function fetchEventsFromUsers() {
+  async.each(users, function (user, next) {
+    crawlUserEvents(user, function(){
       next();
     });
   });
 }
 
+function fetchEventsFromUsers2() {
+  Events.find({
+    start_time: {
+      $gte: date
+    }
+  }, function(err, evs) {
+    var creatorsIds = [];
+    async.each(evs, function (ev, next) {
+      if (ev.creator) {
+        if (creatorsIds.indexOf(ev.creator.id) < 0) {
+          creatorsIds.push(ev.creator.id);
+
+          graph.get(ev.creator.id, function(err, res) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+
+            crawlUserEvents(res.username, function(){
+              next();
+            });
+          });
+        }
+      }
+    });
+  });
+}
+
+function fetchEventsFromLocations() {
+  Events.find({
+    start_time: {
+      $gte: date
+    }
+  }, function(err, evs) {
+    var locations = [];
+    async.each(evs, function (ev, next) {
+      if (locations.indexOf(ev.location) < 0) {
+        locations.push(ev.location);
+        searchEventsFromKeyword(ev.location);
+      }
+    });
+  });
+}
+
 function updateAttendees() {
-  Events.find({}, function(err, evs) {
+  var date = new Date();
+
+  date.setSeconds(0);
+  date.setMinutes(0);
+  date.setHours(0);
+
+  Events.find({
+    start_time: {
+      $gte: date
+    }
+  }, function(err, evs) {
     evs.forEach(function(ev) {
       var query = {
         event_attending: "SELECT uid FROM event_member WHERE eid =" + ev.eid + " and rsvp_status = 'attending' LIMIT 50000",
