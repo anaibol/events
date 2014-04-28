c4f08f80fdcc386b9a1ffadf864b97777f8a6188
Rester-Libre <resterlibreon@gmail.com>
2014-04-28 21:35:08 +0200

    Python parse 3rd party- Function version -url-html

--- parse_3rdparties/parse_and_get_event_links_v2.py
+++ parse_3rdparties/parse_and_get_event_links_v2.py
@@ -30,30 +30,53 @@ limit = int(options.limit)
 
 i = 0
 
+def get_fb_id_from_links_in_lines(lines) :
+    for line in lines :
+        m_info_get_url_id_facebook = re.search(r"(facebook\.com\/events/([0-9]+))", line)
+
+        if (m_info_get_url_id_facebook) :
+            #print " FOUND : " + line
+
+            url = m_info_get_url_id_facebook.group(1)
+            id = m_info_get_url_id_facebook.group(2)
+            print " FOUND : " + id
+
+            url = "http://wooeva.com/import/event/" + id
+            if verbose : 
+                print url
+            print "Loading url : "
+            data = urllib.urlopen(url)
+            print data.read()
+        else :
+            sys.stdout.write(",")
+            sys.stdout.flush()
+
+    print "DPT FINISHED"
+
 if filename != "" :
     with open(filename, 'r') as f:
         lines = f.readlines()
+    get_fb_id_from_links_in_lines(lines)
+    print "WE SHOULD EXIT HERE !?!?!"
 else :
     data = urllib.urlopen(options.url)
-    lines = data.read()
-
-# ca c'est parce que je suis parresseux et ne veut pas ramener toutes les lignes du bas sur la gauche
-if 1 :
-    #while (line = f.read()) :
-    for line in lines :
+    lines = []
+    for line in data :
         i = i + 1
         if (limit > 0 and i > limit) :
             break
 
         m_info_timestamp = re.search(r"(([0-9]{2})\/([A-Z][a-z]{2})\/([0-9]{4}):([0-9]{2}):([0-9]{2}):([0-9]{2}))", line)
 
-
 #        m_info_get_url = 
         # just facebook VR 28-4-14
         #m_info_get_url_id_facebook = re.search(r"(facebook)", line)
 #"(\.)"
-        m_info_get_url_id_facebook = re.search(r"(facebook\.com\/events/([0-9]+))", line)
+        m_info_get_url_id_facebook = re.search(r"(/index\.php/dpt/[0-9]{2})", line)
+        # m_info_get_url_id_facebook = re.search(r"(/index\.php/([A-Z]{1}[a-z]+))", line) 
 #https://www.facebook.com/events/531327140280680/
+
+        #print line
 
         #if (m_info_photo_id_tag and m_info_timestamp) :
         if (m_info_get_url_id_facebook) :
@@ -62,34 +85,22 @@ if 1 :
             #tags = m_info_photo_id_tag.group(2)
 
             url = m_info_get_url_id_facebook.group(1)
-            id = m_info_get_url_id_facebook.group(2)
-            print " id : " + id
+            print " FOUND : " + url
 
-      #      date_string = m_info_timestamp.group(1)
+            # ici on va dans le fichier sous jacent
+            url = "http://salsa.faurax.fr" + url 
 
-      #      date_tagged = datetime.datetime.strptime(date_string, '%d/%b/%Y:%H:%M:%S')
+            print " LOADING LINE FROM :" + url
 
-#            day = m_info_timestamp.group(1)
-#            month = m_info_timestamp.group(2)
-#            year = m_info_timestamp.group(3)
-#            hour = m_info_timestamp.group(4)
-#            minute = m_info_timestamp.group(5)
-#            second = m_info_timestamp.group(6)
+            data2 = urllib.urlopen(url)
+            lines2 = []
+            for l in data2 :
+                lines2.append(l)
+            print "Calling get_fb_id_from_links_in_lines with " + str(len(lines2)) + " ! "
+            get_fb_id_from_links_in_lines(lines2)
 
-            #lastModifiedDate = datetime.datetime.strptime(date_string, '%a, %d %b %Y %H:%M:%S %Z')
-
-            url = "http://wooeva.com/import/event/" + id
-# + "&timestamp=" + date_tagged.strftime("%Y-%m-%d") + "+" + date_tagged.strftime("%H:%M:%S")
-            if verbose : 
-                print url
-            print "Loading url : "
-            data = urllib.urlopen(url)
-            print data.read()
-            #if verbose :
-            #    print data.read()
-#            print "."
         else :
-            sys.stdout.write(",")
+            sys.stdout.write("-")
             sys.stdout.flush()
             #print "NOT Found : " + line
 
