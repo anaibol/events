b49fd364f79099a02720071a0a4d86e1e212a810
anaibol <juananibal.micheli@gmail.com>
2014-05-29 14:33:40 -0300

    Fix

--- app/routes/api.js
+++ app/routes/api.js
@@ -119,6 +119,11 @@ module.exports = function(req, res) {
 
                 break;
 
+              case 'festival':
+                query.festival = true;
+
+                break;
+
               case 'free':
                 query["price.num"] = 0;
 
