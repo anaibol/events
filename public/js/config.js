0b1e4a52800f6e2ab3db4bc0bf12b494c4c35d21
anaibol <juananibal.micheli@gmail.com>
2014-07-16 15:33:56 -0300

    Update for prod

--- public/js/config.js
+++ public/js/config.js
@@ -14,121 +14,121 @@ app.config(function($locationProvider, $stateProvider) {
 // ]);
 
 
-  $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: '/views/events/home.html',
-      controller: 'EventsCtrl'
-    })
-    .state('myEvents', {
-      url: '/me/events',
-      templateUrl: '/views/events/home.html',
-      controller: 'EventsCtrl'
-    })
-    .state('userEvents', {
-      url: '/:user/events',
-      templateUrl: '/views/events/home.html',
-      controller: 'EventsCtrl'
-    })
-    .state('worldwide', {
-      url: '/worldwide',
-      templateUrl: '/views/events/home.html',
-      controller: 'EventsCtrl'
-    })
-    .state('eventsByDate', {
-      url: '/date/:date',
-      templateUrl: '/views/events/home.html',
-      controller: 'EventsCtrl'
-    })
-    .state('popular', {
-      url: '/popular',
-      templateUrl: '/views/events/home.html',
-    })
-    .state('festival', {
-      url: '/festival',
-      templateUrl: '/views/events/home.html',
-    })
-    .state('free', {
-      url: '/free',
-      templateUrl: '/views/events/home.html'
-    })
-    .state('today', {
-      url: '/today',
-      templateUrl: '/views/events/home.html'
-    })
-    .state('weekend', {
-      url: '/weekend',
-      templateUrl: '/views/events/home.html'
-    })
-    .state('create', {
-      url: '/create',
-      templateUrl: '/views/events/create.html'
-    })
-    .state('edit', {
-      url: '/:eventId/edit',
-      templateUrl: '/views/events/create.html'
-    })
-    .state('view', {
-      url: '/:slug',
-      templateUrl: '/views/events/view.html',
-      controller: 'EventCtrl'
-    });
+  // $stateProvider
+  //   .state('home', {
+  //     url: '/',
+  //     templateUrl: '/views/events/home.html',
+  //     controller: 'EventsCtrl'
+  //   })
+  //   .state('myEvents', {
+  //     url: '/me/events',
+  //     templateUrl: '/views/events/home.html',
+  //     controller: 'EventsCtrl'
+  //   })
+  //   .state('userEvents', {
+  //     url: '/:user/events',
+  //     templateUrl: '/views/events/home.html',
+  //     controller: 'EventsCtrl'
+  //   })
+  //   .state('worldwide', {
+  //     url: '/worldwide',
+  //     templateUrl: '/views/events/home.html',
+  //     controller: 'EventsCtrl'
+  //   })
+  //   .state('eventsByDate', {
+  //     url: '/date/:date',
+  //     templateUrl: '/views/events/home.html',
+  //     controller: 'EventsCtrl'
+  //   })
+  //   .state('popular', {
+  //     url: '/popular',
+  //     templateUrl: '/views/events/home.html',
+  //   })
+  //   .state('festival', {
+  //     url: '/festival',
+  //     templateUrl: '/views/events/home.html',
+  //   })
+  //   .state('free', {
+  //     url: '/free',
+  //     templateUrl: '/views/events/home.html'
+  //   })
+  //   .state('today', {
+  //     url: '/today',
+  //     templateUrl: '/views/events/home.html'
+  //   })
+  //   .state('weekend', {
+  //     url: '/weekend',
+  //     templateUrl: '/views/events/home.html'
+  //   })
+  //   .state('create', {
+  //     url: '/create',
+  //     templateUrl: '/views/events/create.html'
+  //   })
+  //   .state('edit', {
+  //     url: '/:eventId/edit',
+  //     templateUrl: '/views/events/create.html'
+  //   })
+  //   .state('view', {
+  //     url: '/:slug',
+  //     templateUrl: '/views/events/view.html',
+  //     controller: 'EventCtrl'
+  //   });
 
 
-  // $routeProvider.
-  // when('/', {
-  //   templateUrl: '/views/events/home.html'
-  // }).
-  // when('/me/events', {
-  //   templateUrl: '/views/events/home.html',
-  //   controller: 'EventsCtrl'
-  // }).
-  // when('/:user/events', {
-  //   templateUrl: '/views/events/home.html',
-  //   controller: 'EventsCtrl'
-  // }).
-  // when('/worldwide', {
-  //   templateUrl: '/views/events/home.html',
-  //   controller: 'EventsCtrl'
-  // }).
-  // when('/date/:date', {
-  //   templateUrl: '/views/events/home.html',
-  //   controller: 'EventsCtrl'
-  // }).
-  // when('/popular', {
-  //   templateUrl: '/views/events/home.html',
-  // }).
-  // when('/festival', {
-  //   templateUrl: '/views/events/home.html',
-  //   controller: 'EventsCtrl' // rapha add
-  // }).
-  // when('/free', {
-  //   templateUrl: '/views/events/home.html'
-  // }).
-  // when('/today', {
-  //   templateUrl: '/views/events/home.html'
-  // }).
-  // when('/weekend', {
-  //   templateUrl: '/views/events/home.html'
-  // }).
-  // when('/create', {
-  //   templateUrl: '/views/events/create.html'
-  // }).
-  // when('/:eventId/edit', {
-  //   templateUrl: '/views/events/create.html'
-  // }).
-  // when('/:slug', {
-  //   templateUrl: '/views/events/view.html',
-  //   controller: 'EventCtrl'
-  // })
+  $routeProvider.
+  when('/', {
+    templateUrl: '/views/events/home.html'
+  }).
+  when('/me/events', {
+    templateUrl: '/views/events/home.html',
+    controller: 'EventsCtrl'
+  }).
+  when('/:user/events', {
+    templateUrl: '/views/events/home.html',
+    controller: 'EventsCtrl'
+  }).
+  when('/worldwide', {
+    templateUrl: '/views/events/home.html',
+    controller: 'EventsCtrl'
+  }).
+  when('/date/:date', {
+    templateUrl: '/views/events/home.html',
+    controller: 'EventsCtrl'
+  }).
+  when('/popular', {
+    templateUrl: '/views/events/home.html',
+  }).
+  when('/festival', {
+    templateUrl: '/views/events/home.html',
+    controller: 'EventsCtrl' // rapha add
+  }).
+  when('/free', {
+    templateUrl: '/views/events/home.html'
+  }).
+  when('/today', {
+    templateUrl: '/views/events/home.html'
+  }).
+  when('/weekend', {
+    templateUrl: '/views/events/home.html'
+  }).
+  when('/create', {
+    templateUrl: '/views/events/create.html'
+  }).
+  when('/:eventId/edit', {
+    templateUrl: '/views/events/create.html'
+  }).
+  when('/:slug', {
+    templateUrl: '/views/events/view.html',
+    controller: 'EventCtrl'
+  })
 
-  // .
-  // .state('', {'/', {
-  // //  templateUrl: '/views/index.html'
-  // }).
-  // otherwise({
-  //   redirectTo: '/'
-  // });
+  .
+  .state('', {'/', {
+  //  templateUrl: '/views/index.html'
+  }).
+  otherwise({
+    redirectTo: '/'
+  });
 });
 
   app.config(function (datepickerPopupConfig) {
