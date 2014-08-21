8115d84fabc52b10e286bae6cfd164dac796b2a3
anaibol <juananibal.micheli@gmail.com>
2014-08-21 20:08:03 -0300

    Rsvp synced with facebook, url changes with slug and id

--- app/controllers/rsvp.js
+++ app/controllers/rsvp.js
@@ -5,7 +5,7 @@ var graph = require('fbgraph');
 var accessToken = 'CAAGPsrwaxr4BAJeLRPDmSmvGdDZBRbs5kmpyge2XZBZBvbLpy36rPCY4uA0nTVu4O1STnUMWpv9UKpoA9YzwJyT9RYROZA0oVvtxPlzEaWuYZAahfC2vEbB6wIEQOZCPPGF8iUPlzQiqrQ7Pzj9bzgFoUfDbWqFF8uO8NrOFE6CaPbMSkZBOM65vBGTWs4sGnNAI3qnHK1YghqCull98SFH';
 
 exports.getUserStatus = function(req, res) {
-  graph.setAccessToken(accessToken);
+  graph.setAccessToken(req.user.accessToken);
 
   var query = "SELECT rsvp_status, inviter, inviter_type FROM event_member WHERE uid=me() AND eid=" + req.params.eid;
 
