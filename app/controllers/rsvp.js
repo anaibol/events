97b73bc43682b70e094984f993614f83b2307017
anaibol <juananibal.micheli@gmail.com>
2014-09-01 12:08:41 -0300

    ´Fix_attending´

--- app/controllers/rsvp.js
+++ app/controllers/rsvp.js
@@ -33,7 +33,7 @@ exports.getEventAttendings = function(req, res) {
 exports.setAttending = function(req, res) {
   graph.setAccessToken(req.user.accessToken);
 
-  graph.post('/' + req.params.eid + '/' + req.body.attendingStatus + '?access_token=' + accessToken, function(err, result) {
+  graph.post('/' + req.params.eid + '/' + req.body.attendingStatus, function(err, result) {
     if (err) {
       res.json(err);
     }
