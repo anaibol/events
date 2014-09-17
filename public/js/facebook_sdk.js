51b8e6299c58db5fcd9f9aa90fdaefdc17510a1b
Junit38 <jmery@student.42.fr>
2014-09-17 16:34:52 +0200

    #141 Facebook SDK pluged in

---
+++ public/js/facebook_sdk.js
@@ -0,0 +1,17 @@
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '439472799532734|q2yZ3bxPv8magGScTA672Ab-x7Y',
+          xfbml      : true,
+          version    : 'v2.0'
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+    </script>
