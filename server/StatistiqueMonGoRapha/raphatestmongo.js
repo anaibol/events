125313ab07b3d58ee84efd0b759486ef91388548
Rester-Libre <resterlibreon@gmail.com>
2014-10-21 19:21:52 +0200

    statistiques mongoRapha

--- server/StatistiqueMonGoRapha/raphatestmongo.js
+++ server/StatistiqueMonGoRapha/raphatestmongo.js
@@ -1,4 +1,4 @@
-global.rootDir = __dirname + '/';
+global.rootDir = __dirname + '/../';
 global.configDir = rootDir + 'config';
 global.config = require(configDir + '/env/' + process.env.NODE_ENV + '.js');
 
@@ -12,12 +12,18 @@ var Dateminuit = new Date();
   Dateminuit.setSeconds(0);
   Dateminuit.setMinutes(0);
   Dateminuit.setHours(0);
+
+// Date d'aujourd'hui à minuit
 console.log(Dateminuit);
 
 Events.count({ saved: { $gt: Dateminuit }},function(e, count){
-	console.log("nombre d'event sauvés aujourd'hui après minuit: " + count)
+	console.log("nombre d'event importés aujourd'hui après minuit: " + count)
 });
 
-//Events.count($or [{ start_date: { $gt: Dateminuit }}, { start_date: { $gt: Dateminuit }}],function(e, count){
-//	console.log("nombre d'event live " + count)
-//});
+Events.count({$or: [{ start_time: { $gt: Dateminuit }}, {end_time: { $gt: Dateminuit }}]},function(e, count){
+	console.log("nombre d'event live EN VIE " + count)
+});
+
+Events.count({ updated: { $gt: Dateminuit }},function(e, count){
+	console.log("nombre d'event updaté aujourd'hui après minuit: " + count)
+});
