43f1c5f94995644408c69fcf533992cc283acb74
Derhu <tristan.roby@epitech.eu>
2014-11-12 12:09:18 +0100

    date test

--- client/js/config.js
+++ client/js/config.js
@@ -90,9 +90,6 @@ app.run(function($rootScope, $state, $stateParams, $localStorage, amMoment, ezfb
   $rootScope.lang = navigator.language || navigator.userLanguage;
 
   amMoment.changeLocale($rootScope.lang);
-  angular.module('angularMoment').value('angularMomentConfig', {
-    timezone: 'Europe/Dublin'
-  });
   $rootScope.today = new Date();
 
   $rootScope.today.setSeconds(0);
