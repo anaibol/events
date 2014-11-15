10b6dcdb36e4da9df0cddb5210c51c1882ae9422
anaibol <juananibal.micheli@gmail.com>
2014-11-15 14:41:34 -0300

    Fix timezone

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -1,15 +1,34 @@
 module.exports = {
-  db: "mongodb://localhost/wooepa-dev",
+  db: 'mongodb://admin:blacksabbath@candidate.40.mongolayer.com:10363/Wooepa',
   port: 3000,
   app: {
-    name: "Wooepa - Development",
-    accessToken: "1511193072439143|B3CXLPHQjOUyQ6Bu1wbdyLxJEwQ"
+    name: 'Wooepa - Test',
+    accessToken: '439472799532734|q2yZ3bxPv8magGScTA672Ab-x7Y'
   },
   facebook: {
-    clientID: "1511193072439143",
-    clientSecret: "2d463b32df69fd1f1e398868705ff0eb",
-    //clientID: "439472799532734",
-    //clientSecret: "6e940b23fdbf539939dffbe479678623",
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    clientID: '439472799532734',
+    clientSecret: '6e940b23fdbf539939dffbe479678623',
+    callbackURL: 'http://wooepa.com/auth/facebook/callback'
   }
+  /*,
+  twitter: {
+    clientID: 'CONSUMER_KEY',
+    clientSecret: 'CONSUMER_SECRET',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback'
+  },
+  github: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/github/callback'
+  },
+  google: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  },
+  linkedin: {
+    clientID: 'API_KEY',
+    clientSecret: 'SECRET_KEY',
+    callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+  }*/
 };
