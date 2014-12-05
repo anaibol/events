8fa13354aa07f907807da782af708030e82b7622
Derhu <tristan.roby@epitech.eu>
2014-12-05 16:12:43 +0100

    name of user live events

--- client/js/controllers/myEventsCtrl.js
+++ client/js/controllers/myEventsCtrl.js
@@ -6,14 +6,28 @@ app.controller('myEventsCtrl', function($scope, moment, $http) {
   if (url == "")
     url = $scope.user.facebook.id;
   else
-  {
-    var i = url.length;
-    if (url[i] == '/')
-      --i;
-    while (url[i] != '/')
-      --i;
-    url = url.substring(i + 1, url.length);
-  }
+    {
+      var i = url.length;
+      if (url[i] == '/')
+        --i;
+      while (url[i] != '/')
+        --i;
+      url = url.substring(i + 1, url.length);
+    }
+  $http.get('/api/user/' + url).success(function(data){
+    if (data)
+    {
+      $http({url: '/api/me/events/' + url, method: 'GET'}).success(function(my_evs){
+        if (my_evs)
+        {
+          $scope.events = my_evs;
+          $scope.events.printmessage = "'s Live Events! " + my_evs.length + ' events';
+          $scope.events.printname = data.facebook.name;
+          $scope.getTags();
+        }
+      });
+    }
+  });
   $scope.getTags = function() {
     $scope.tags = _.uniq([].concat.apply([], _.pluck($scope.events, 'tags'))).sort();
     };
@@ -23,8 +37,4 @@ app.controller('myEventsCtrl', function($scope, moment, $http) {
         location.reload();
       });
     };
-  $http({url: '/api/me/events/' + url, method: 'GET'}).success(function(my_evs){
-    $scope.events = my_evs;
-    $scope.getTags();
-  });
 });
