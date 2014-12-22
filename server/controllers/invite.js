48bab7e4d04597c5e8371a6bedce5046f546dd62
anaibol <juananibal.micheli@gmail.com>
2014-12-22 15:33:10 -0300

    invitations redirect fix

--- server/controllers/invite.js
+++ server/controllers/invite.js
@@ -6,10 +6,10 @@ var db = require('monk')(config.db);
 var Invitations = db.get('invitations');
 
 exports.sendInvitations = function(req, res) {
- graph.post('/' + req.params.eid + '/invited/' + req.params.uids + '?access_token=' + req.user.accessToken, function(err, result) {
-    if (err) {
-      res.json(err);
-    } else {
+ // graph.post('/' + req.params.eid + '/invited/' + req.params.uids + '?access_token=' + req.user.accessToken, function(err, result) {
+ //    if (err) {
+ //      res.json(err);
+ //    } else {
       var invitation = {
         to: req.body.uids,
         eid: req.body.eid,
@@ -26,6 +26,6 @@ exports.sendInvitations = function(req, res) {
       Invitations.insert(invitation);
 
       res.send(true);
-    }
+    // }
   });
 };
