d73dbd68c0bbd206c7b69b3dd28fbb7e3146034b
anaibol <juananibal.micheli@gmail.com>
2014-12-22 02:35:16 -0300

    redirect fix

--- server/controllers/invite.js
+++ server/controllers/invite.js
@@ -13,13 +13,19 @@ exports.sendInvitations = function(req, res) {
  //    else {
  //    if (req.user) {
       Inv.giveInvitePoints(req.body.eid, req.user.facebook.id, req.body.uids);
-      Invitations.insert({
-        from: req.user.facebook.id,
+
+      var invitation = {
         to: req.body.uids,
         eid: req.body.eid,
         requestId: req.body.requestId,
         url: req.body.url
-      });
+      };
+
+      if (req.user) {
+        invitation.from = req.user.facebook.id;
+      }
+
+      Invitations.insert(invitation);
 
   //     res.json();
   //   }
