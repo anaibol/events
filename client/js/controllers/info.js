1694ea7d2381466fb3cf9e121433c1881b15e1c8
Derhu <tristan.roby@epitech.eu>
2014-12-22 12:52:46 +0100

    boostyourevent

--- client/js/controllers/info.js
+++ client/js/controllers/info.js
@@ -16,6 +16,19 @@ app.controller('infoCtrl', function($scope, $sce) {
   $scope.midbanner = $sce.trustAsResourceUrl('/img/discoverevents/midbanner.jpg');
   $scope.loupemailbot = $sce.trustAsResourceUrl('/img/discoverevents/loupemailbot.png');
   $scope.footer = $sce.trustAsResourceUrl('/img/discoverevents/footer.jpg');
-  $scope.headerboost = $sce.trustAsResourceUrl('/img/boostyourevent/header.png');
-  $scope.boostmidbanner = $sce.trustAsResourceUrl('/img/boostyourevent/midbanner.png');
+ 
+  $scope.headerboost = $sce.trustAsResourceUrl('/img/boostyourevent/header.jpg');
+  $scope.boostmidbanner = $sce.trustAsResourceUrl('/img/boostyourevent/midbanner.jpg');
+  $scope.boostfooter = $sce.trustAsResourceUrl('/img/boostyourevent/footer.jpg');
+  $scope.muchmore = $sce.trustAsResourceUrl('/img/boostyourevent/muchmore.jpg');
+  $scope.targetting = $sce.trustAsResourceUrl('/img/boostyourevent/targetting.jpg');
+  $scope.contactus = $sce.trustAsResourceUrl('/img/boostyourevent/contactus.jpg');
+  $scope.boostredarrow = $sce.trustAsResourceUrl('/img/boostyourevent/redarrow.png');
+  $scope.encoche = $sce.trustAsResourceUrl('/img/boostyourevent/encoche.png');
+  $scope.purplearrow = $sce.trustAsResourceUrl('/img/boostyourevent/purplearrow.png');
+  $scope.hornbutton = $sce.trustAsResourceUrl('/img/boostyourevent/hornbutton.png');
+  $scope.emailbutton = $sce.trustAsResourceUrl('/img/boostyourevent/email.png');
+  $scope.instagrambutton = $sce.trustAsResourceUrl('/img/boostyourevent/instagram.png');
+  $scope.facebookbutton = $sce.trustAsResourceUrl('/img/boostyourevent/facebook.png');
+  $scope.twitterbutton = $sce.trustAsResourceUrl('/img/boostyourevent/twitter.png');
 });
