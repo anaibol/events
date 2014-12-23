58ab2a39105ed7ec84c380b9b3abeebb66e21e9d
Derhu <tristan.roby@epitech.eu>
2014-12-23 13:11:43 +0100

    change db passw

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(port);
-httpsServer.listen(43);
+httpsServer.listen(443);
 
 console.log('Express app started on port ' + port);
 
