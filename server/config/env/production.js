7ddf2d8236d9af0ea3053091b5bb1cfac3909398
Derhu <tristan.roby@epitech.eu>
2014-12-23 13:04:34 +0100

    change db passw

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: 'mongodb://admin:blacksabbath@candidate.40.mongolayer.com:10363/Wooepa',
+  db: 'mongodb://admin:blacksabbath@candidate.43.mongolayer.com:10086/Wooepa',
   port: 3000,
   app: {
     name: 'Wooepa - Production',
